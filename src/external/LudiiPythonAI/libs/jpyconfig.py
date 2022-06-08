001c672e2b0a97625552477d0060b81d687d3e65
Enzo DURAND <durande@newmail.lip6.fr>
2022-06-08 12:37:25 +0200

    working on debian

--- src/external/LudiiPythonAI/libs/jpyconfig.py
+++
@@ -1,8 +0,0 @@
-# Created by 'jpyutil.py' tool on 2022-06-07 11:50:05.547099
-# This file is read by the 'jpyutil' module in order to load and configure the JVM from Python
-java_home = 'C:\\Programmes\\Java\\jdk-18.0.1.1'
-jvm_dll = 'C:\\Programmes\\Java\\jdk-18.0.1.1\\bin\\server\\jvm.dll'
-jvm_maxmem = None
-jvm_classpath = []
-jvm_properties = {}
-jvm_options = []

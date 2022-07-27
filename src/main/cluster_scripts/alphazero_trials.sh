0d26e5ef618a0a887ebe88ca7bd1ab839242a3b5
hanzopgp <enzodurandcontact@gmail.com>
2022-07-27 14:00:26 +0200

    trying on multiple nodes

--- src/main/cluster_scripts/alphazero_trials.sh
+++ src/main/cluster_scripts/alphazero_trials.sh
@@ -1,4 +1,4 @@
 export PATH=$PATH:/data/vittaut/apache-ant-1.10.12/bin 
 conda init bash
 conda activate alphazero
-ant run_trials -Dforce_vanilla=False
+ant run_trials -Dforce_vanilla=False & ant run_trials -Dforce_vanilla=False & wait
